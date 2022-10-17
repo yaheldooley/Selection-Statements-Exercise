a55352fd652223ed25ce9c3d989ae819210cf58d
yahel dooley <ydooley@gmail.com>
2022-10-17 15:33:22 -0700

    Completed Exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+			var r = new Random();
+			int favNumber = r.Next(1, 200);
+
+			bool solved = false;
+			while (!solved)
+			{
+				Console.WriteLine("Try to guess my favorite number!");
+				var userInput = int.Parse(Console.ReadLine());
+
+				if (userInput < favNumber)
+				{
+					Console.WriteLine("Too low!");
+				}
+				else if (userInput > favNumber)
+				{
+					Console.WriteLine("Too high!");
+				}
+				else solved = true;
+			}
+			Console.WriteLine("Nevermind");
+		}
     }
 }
